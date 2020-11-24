43e9b83f3f35762ad833a6bcba6338a521b5cb37
Michael Zachor <mfz226@nyu.edu>
2020-11-24 15:44:46 -0500

    create account and login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
@@ -23,15 +23,73 @@ const firebaseConfig = {
 };
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false); // bool to determine if logged in
+  const [loading, setLoading] = useState(true); // is page loading? (don't show info before its' loaded)
+  // const [userInformation, setUserInformation] = useState({});
+
+  // ensure app is initialized when it's ready
+  useEffect(() => {
+    // initializes firebase, but only if it hasn't been initialized before
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    console.log('firebase initialized');
+  }, [firebaseConfig])
+
+  // function for logging in
+  function LoginFunction(e) {
+    // This is what you'll run when you wanna log in
+    e.preventDefault(); // stop the form from submitting as a normal html form
+    const email = e.currentTarget.loginEmail.value;
+    const password = e.currentTarget.loginPassword.value;
+
+    console.log({ email, password });
+
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(function(response) {
+      console.log('LOGIN RESPONSE', response);
+      setLoggedIn(true);
+    })
+    .catch(function(error) {
+      console.log('LOGIN ERROR', error);
+      // setLoggedIn(false); // it already is by default
+    });
+  }
+  // function for logging out
+  function LogoutFunction() {
+    // Run this when you wanna log out
+  }
+  // function for creating an account
+  function CreateAccountFunction(e) {
+    // Run this when you create an account
+    e.preventDefault();
+    const email = e.currentTarget.createEmail.value;
+    const password = e.currentTarget.createPassword.value;
+
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(function(response) {
+      console.log('VALID ACCOUNT CREATED FOR:', email, response);
+      setLoggedIn(true);
+    })
+    .catch(function(error) {
+      console.log('ACCOUNT CREATION FAILED', error);
+    })
+  }
+  console.log({ loggedIn });
+
   return (
   <div className="App">
-    <Header />
+    <Header loggedIn={loggedIn} LogoutFunction={LogoutFunction}/>
     <Router>
       <Route exact path="/login">
-        <Login />
+        <Login LoginFunction={LoginFunction} />
       </Route>
       <Route exact path="/create-account">
-        <CreateAccount />
+        <CreateAccount CreateAccountFunction={CreateAccountFunction}/>
       </Route>
       <Route exact path="/">
         <UserProfile />
